977c5025cf385041144b424dfd40a0c0ddb4b743
Bolaji <rotibi.m@husky.neu.edu>
2019-03-10 00:34:58 -0500

    Well just a few small edits to the css and dashboard

--- routes/users.js
+++ routes/users.js
@@ -87,7 +87,7 @@ router.post('/login', (req, res, next) => {
 // Logout
 router.get('/logout', (req, res) => {
   req.logout();
-  req.flash('success_msg', 'You are logged out');
+  req.flash('success_msg', 'You have been logged out');
   res.redirect('/users/login');
 });
 
