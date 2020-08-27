072e881ff3d33230757a04f9b4afd9b2b1e88b79
Mobolaji Rotibi <brotibi@gmail.com>
2020-08-27 14:45:44 -0400

    Update keys.js
    security

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
-//db = 'mongodb+srv://username:serverName/test?retryWrites=true'
-
-db = "mongodb+srv://test:8WEXzDy4XtM5eE0N@cluster0-2gs89.azure.mongodb.net/test?retryWrites=true&w=majority"
+db = 'mongodb+srv://username:serverName/test?retryWrites=true'
 
 module.exports = db;
